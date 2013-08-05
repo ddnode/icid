3f4707ecdf82a88382f852bdb8a5701f321cf148
Boy <andong135@126.com>
2013-08-05 22:20:48 +0800

    add icid theme

---
+++ sites/all/themes/icid/templates/page.tpl.php
@@ -0,0 +1,55 @@
+<div id="page">
+  <div class="inner clearfix">
+    <div id="header">
+      <div class="inner clearfix">
+        <?php print render($page['header']); ?>
+      </div>
+    </div>
+    <?php if ($messages): ?>
+      <div id="messages">
+        <div class="inner clearfix">
+          <?php print $messages; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+    <div id="main">
+      <div class="inner clearfix">
+
+        <?php if($page['sidebar_first']): ?>
+          <div id="sidebar-first">
+            <div class="inner clearfix">
+              <?php print render($page['sidebar_first']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+
+        <div id="content">
+          <div class="inner clearfix">
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+              <h1 class='page-title'><?php print $title ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print render($page['help']); ?>
+            <?php print render($page['content']); ?>
+          </div>
+        </div>
+
+        <?php if($page['sidebar_second']): ?>
+          <div id="sidebar-second">
+            <div class="inner clearfix">
+              <?php print render($page['sidebar_second']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+
+    <div id="footer">
+      <div class="inner clearfix">
+        <?php print render($page['footer']); ?>
+        <div class="copyright">&copy; 2013 International Conference on Interaction Design</div>
+      </div>
+    </div>
+  </div>
+</div>
