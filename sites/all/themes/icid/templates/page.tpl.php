07d94ef5bc876f5f016dd6d0ed9a582e8f7d00ac
Boy <andong135@126.com>
2013-08-06 21:59:34 +0800

    make menu style

--- sites/all/themes/icid/templates/page.tpl.php
+++ sites/all/themes/icid/templates/page.tpl.php
@@ -25,11 +25,6 @@
 
         <div id="content">
           <div class="inner clearfix">
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?>
-              <h1 class='page-title'><?php print $title ?></h1>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
             <?php print render($page['help']); ?>
             <?php print render($page['content']); ?>
           </div>
