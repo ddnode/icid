4b612af571a70fef4eb61b3d1509e6f9c8af510e
Boy <andong135@126.com>
2013-08-14 21:13:37 +0800

    redirect not login user to user login page

--- sites/all/themes/icid/template.php
+++ sites/all/themes/icid/template.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * Implements hook_preprocess_page().
+ */
+function icid_preprocess_page(&$variables) {
+  if (current_path() == 'node/20' && user_is_anonymous()) {
+    drupal_goto('user/login', array('query'=>drupal_get_destination()));
+  }
+}
