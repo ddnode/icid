c3bb983c0f7cf080ac3cc70cc8b3462d0542ba99
Boy <andong135@126.com>
2013-08-17 18:45:24 +0800

    add sale rules

--- sites/all/modules/webform/templates/webform-submission.tpl.php
+++ sites/all/modules/webform/templates/webform-submission.tpl.php
@@ -18,73 +18,51 @@
 <?php
   $user = user_load($renderable['#submission']->uid);
   $mail = $user->mail;
-
-  $main_conference_num = $renderable['conference_registration_fee']['main_conference_num']['#value'];
-  $workshop_count = isset($renderable['workshop_fees']['workshop_count']['#value']) ? $renderable['workshop_fees']['workshop_count']['#value'] : 0;
-  $ticket_count = isset($renderable['additional_banquet_ticket']['ticket_count']['#value']) ? $renderable['additional_banquet_ticket']['ticket_count']['#value'] : 0;
-  $apply_for_group_price = isset($renderable['groups']['apply_for_group_price']['#value']) ? $renderable['groups']['apply_for_group_price']['#value'] : '';
-  $registration_type = $renderable['registration_type']['#value'][0];
-  $number_of_people_in_the_group = isset($renderable['groups']['number_of_people_in_the_group']['#value']) ? $renderable['groups']['number_of_people_in_the_group']['#value'] : 0;
-
-  $time = $renderable['#submission']->submitted;
-  $cf_fee = $time < strtotime("2013-10-10") ? 1800 : 2000;
-  $cf_fee = $registration_type == 1 ? 300 : $cf_fee;
-  $ws_fee = 800;
-  $bt_fee = 80;
-  $cf_fees = $cf_fee * $main_conference_num;
-  $ws_fees = $ws_fee * $workshop_count;
-  $bt_fees = $bt_fee * $ticket_count;
-  if ($apply_for_group_price && $registration_type == 0) {
-    $cf_fee = $time < strtotime("2013-9-30") ? 1800 : 2000;
-    $num = $number_of_people_in_the_group >= 5 ? $number_of_people_in_the_group - 1 : $number_of_people_in_the_group;
-    $cf_fees = $cf_fees * $num;
-  }
-  $total = $cf_fees + $ws_fees + $bt_fees;
+  $name = $renderable['first_name']['#value'] . ' ' . $renderable['last_name']['#value'];
+  $registration_type = $renderable['registration_type']['#value']['0'] ? '学生' : '成人';
+  $group_price = isset($renderable['groups']['group_people_num']) ? '是' : '否';
+  $total = $renderable['total_fees']['#markup'];
+  $fees_status = $renderable['fees_status']['#value'][0] ? '已付款' : '未付款';
 ?>
+<h3>非常感谢您参加 2013 年交互设计国际会议，您的注册已完成，以下是详细信息：</h3>
 <table>
   <tr>
-    <td>注册明细<br />Registration List</td>
-    <td>邮箱<br />Email</td>
-    <td>注册项目<br />Registration Type</td>
-    <td>状态<br />Actions</td>
-  </tr>
-  <tr>
-    <td><?php print $renderable['first_name']['#value'] . ' ' . $renderable['last_name']['#value']; ?></td>
+    <td>注册邮箱</td>
     <td><?php print $mail; ?></td>
-    <td></td>
-    <td></td>
+  </tr>
+  <tr>
+    <td>姓名</td>
+    <td><?php print $name; ?></td>
+  </tr>
+  <tr>
+    <td>注册项目</td>
+    <td><?php print $registration_type; ?></td>
+  </tr>
+  <tr>
+    <td>团队价格</td>
+    <td><?php print $group_price; ?></td>
+  </tr>
+  <tr>
+    <td>注册费用</td>
+    <td>RMB <?php print $total; ?>元</td>
+  </tr>
+  <tr>
+    <td>付款状态</td>
+    <td><?php print $fees_status; ?></td>
   </tr>
 </table>
+<p>
+汇款账号<br />
+所有参会费用须以汇款支付。<br />
+户名： 清华大学<br />
+账号： 0200004509089131550<br />
+开户行：工商银行北京分行海淀西区支行<br />
+备注栏务必注明：2013交互设计国际会议注册费用＋会员姓名和编号<br />
+组委会联系人：关琰<br />
+地址：北京市清华大学美术学院信息艺术设计系B435室<br />
+电话：86-01-62798934<br />
+传真：86-01-62798933
+</p>
 
-<table>
-  <tr>
-    <td>付费项目<br />Fees</td>
-    <td>数量<br />Quantity</td>
-    <td>费用<br />Unit Price</td>
-    <td>总结<br />Amount</td>
-  </tr>
-  <tr>
-    <td>ICID大会<br />Main Conference</td>
-    <td><?php print $main_conference_num; ?></td>
-    <td>RMB <?php print $cf_fee; ?></td>
-    <td>RMB <?php print $cf_fees; ?></td>
-  </tr>
-  <tr>
-    <td>主题研讨会 1<br />Workshop 1</td>
-    <td><?php print $workshop_count; ?></td>
-    <td>RMB <?php print $ws_fee; ?></td>
-    <td>RMB <?php print $ws_fees; ?></td>
-  </tr>
-  <tr>
-    <td>附加晚餐入场券<br />Additional Banquet Ticket</td>
-    <td><?php print $ticket_count; ?></td>
-    <td>RMB <?php print $bt_fee; ?></td>
-    <td>RMB <?php print $bt_fees; ?></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td>总费用/Total</td>
-    <td>RMB <?php print $total; ?></td>
-  </tr>
-</table>
+<a href="javascript:window.print(); return false;" class="print">Print</a>
+<a href="javascript:var win = window.open('', '_self'); win.close(); return false;" class="close">Close</a>
