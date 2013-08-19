94742cee8fa89c6fbd550e7b740a96b3e0f38850
Boy <andong135@126.com>
2013-08-19 23:02:03 +0800

    theme user login form

---
+++ sites/all/themes/icid/templates/page--user.tpl.php
@@ -0,0 +1,58 @@
+<div id="page">
+  <div class="inner clearfix">
+    <div id="header">
+      <div class="inner clearfix">
+        <a class="home" href="<?php print $front_page; ?>" >ICID</a>
+        <?php print render($page['header']); ?>
+      </div>
+    </div>
+    <?php if ($messages): ?>
+      <div id="messages">
+        <div class="inner clearfix">
+          <?php print $messages; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+    <div id="main">
+      <div class="inner clearfix">
+
+        <?php if($page['sidebar_first']): ?>
+          <div id="sidebar-first">
+            <div class="inner clearfix">
+              <?php print render($page['sidebar_first']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+
+        <div id="content">
+          <div class="inner clearfix">
+            <?php print render($page['help']); ?>
+            <div class="tabs">
+              <ul class="tabs primary">
+                <li><?php print l('登录<br />Log in', 'user', array('html' => TRUE)); ?></li>
+                <li><?php print l('创建新帐号<br />Create new account', 'user/register', array('html' => TRUE)); ?></li>
+                <li><?php print l('重设密码<br />Request new password', 'user/password', array('html' => TRUE)); ?></li>
+              </ul>
+            </div>
+            <?php print render($page['content']); ?>
+          </div>
+        </div>
+
+        <?php if($page['sidebar_second']): ?>
+          <div id="sidebar-second">
+            <div class="inner clearfix">
+              <?php print render($page['sidebar_second']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+
+    <div id="footer">
+      <div class="inner clearfix">
+        <?php print render($page['footer']); ?>
+        <div class="copyright">&copy; 2013 International Conference on Interaction Design</div>
+      </div>
+    </div>
+  </div>
+</div>
